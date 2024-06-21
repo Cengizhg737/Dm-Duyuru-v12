513af870d54cee029c4bab6f6d0c5bce206028c3
Cengizhg737 <gumetugba235@gmail.com>
2024-06-21 21:50:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -157,5 +157,5 @@ client.on("message", async message => {
   }
 });
 
-client.login(`bottokengir`);
+client.login(`bottokengir`);MTI1MTc5NjI5Njk0MDEzMDMzNg.GpKDE1.F-YWpjW53A4qgWKnhQad3Id2OhSVlv7dTOcl3c
 
